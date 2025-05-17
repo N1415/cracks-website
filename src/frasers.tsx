ee36bdc204c8987c806290f121fd34350db7fa89
Nacho <nacholopezdiazgbb@gmail.com>
2025-05-17 10:16:18 +0700

    frasers try 2

--- src/frasers.tsx
+++ src/frasers.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import FrasersConcepts from './components/frasers/concepts';
 
 function FrasersPage() {
   return (
     <div className="font-lato">
+      <Navbar />
       <FrasersConcepts />
+      <Footer />
     </div>
   );
 }
